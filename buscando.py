5a33b5c97f2f3b954bbbc0aef9355b7c161a110d
kobolddoido <kobolddoido@gmail.com>
2013-12-16 16:11:23 -0200

    antes do fisher

--- buscando.py
+++ buscando.py
@@ -9,8 +9,7 @@ class Th(Thread):
         self.bed_file=bed_file
         self.genes_file=genes_file
     def run(self):
-        print "thread iniciada "
-        print self.num
+        print "thread iniciada ",self.num
         search_mark(self.bed_file,self.genes_file)
 
 
@@ -21,8 +20,9 @@ vetor_beds=listdir("bed_files")
 count_thread=1
 threads_vector=[]
 for bed in vetor_beds:
-    print 'bed_files/'+bed
-    threads_vector.append( Th(count_thread,'bed_files/'+bed,'half_life_AS_menor 3h.txt'))
-    print len(threads_vector)
+    #print 'bed_files/'+bed
+    threads_vector.append( Th(count_thread,'bed_files/'+bed,'half_life_AS_maior 3h.txt'))
+
+    #print len(threads_vector)
     threads_vector[len(threads_vector)-1].start()
     count_thread+=1
--- intersect.py
+++ intersect.py
@@ -1,33 +1,37 @@
 __author__ = 'Lucas'
 import re
 
-def search_mark(arquivo_bed,arquivo_querys):
 
+def search_mark(arquivo_bed, arquivo_querys):
     '''arquivo_bed=bed format file
      arquivo_querys= vetor com querys'''
 
 
 
-        #else:
-        #    print 'teste
-    print arquivo_bed.split("_")[2]
-    saida=open('saida_'+arquivo_bed.split("_")[3]+"_"+arquivo_querys,'w')
-    file=open(arquivo_bed,'r')
+    #else:
+    #    print 'teste
+    #print arquivo_bed.split("_")[2]
+    saida = open('saida_' + arquivo_bed.split("_")[3] + "_" + arquivo_querys, 'w')
+    file = open(arquivo_bed, 'r')
+    encontradas = 0
+    tamanho = len(file.read().split('\n'))
+    file = open(arquivo_bed, 'r')
+
     for linha in file:
-        if len(linha)>0:
+        if len(linha) > 0:
             #print linha
-            chr=linha.split('\t')[0]
-            start=linha.split('\t')[1]
-            end=linha.split('\t')[2]
+            chr = linha.split('\t')[0]
+            start = linha.split('\t')[1]
+            end = linha.split('\t')[2]
             #print chr,int(start),int(end)
-            file_meia_vida=open(arquivo_querys,'r')
+            file_meia_vida = open(arquivo_querys, 'r')
             for linha_meia in file_meia_vida:
                 #print 'entrou1'
-                if len(linha_meia)>0:
+                if len(linha_meia) > 0:
                     #print 'entrou2'
-                    if not re.match('#',linha_meia):
-                        valores=linha_meia.split('\t')
-                        cordenadas=re.split(':|-',valores[12]) #12 em alguns arquivos.Arquivos de
+                    if not re.match('#', linha_meia):
+                        valores = linha_meia.split('\t')
+                        cordenadas = re.split(':|-', valores[12]) #12 em alguns arquivos.Arquivos de
                         #print cordenadas
                         if (chr == cordenadas[0]):
                             #print chr == cordenadas[0]
@@ -35,12 +39,13 @@ def search_mark(arquivo_bed,arquivo_querys):
                             if int(start) > int(cordenadas[1]):
                                 #print 'aa'
                                 if int(end) < int(cordenadas[2]):
-                                    print "\t".join(valores)+'\n'
-                                    print start
-                                    saida.write("\t".join(valores)+'\n')
-
-                        #chr_rna=valores[]
-                        #if
-                        #break
+                                    #print "\t".join(valores)+'\n'
+                                    #print start
+                                    saida.write("\t".join(valores))
+                                    encontradas += 1
+    print  'saida',float(encontradas) / float(tamanho), "saida_" + arquivo_bed.split("_")[3] + "_" + arquivo_querys
+    #chr_rna=valores[]
+    #if
+    #break
 
 
